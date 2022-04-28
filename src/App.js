67d1b7793b631e8b1f0b4d0113b7b16b7921d4c6
mubarizmayilzade <mubarizbm@code.edu.az>
2022-04-28 22:00:37 +0400

    create users and chat boxes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="a0fbbd5c-aa63-42d2-9d81-27e3bd08fa8e"
+            userName="thesaintmarz"
+            userSecret="12345"
+            renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps}/>}
+            />
+    )
+}
+
+export default App ;
